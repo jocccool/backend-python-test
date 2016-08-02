5ff159c0879b2d6dc4d35dd3e0781a13ca7e528a
Jocelyn Gervais <jocelyn_cool@hotmail.com>
2016-08-02 18:45:29 +0000

    TASK 5: As a user I can see my list of todos paginated.

--- alayatodo/views.py
+++ alayatodo/views.py
@@ -1,4 +1,5 @@
 import json
+from math import ceil
 from alayatodo import app
 from flask import (
     g,
@@ -7,6 +8,37 @@ from flask import (
     request,
     session
     )
+
+
+PER_PAGE = 5
+
+
+class Pagination(object):
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2, right_current=5, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or (num > self.page - left_current - 1 and num < self.page + right_current) or num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
 
 
 @app.route('/')
@@ -46,19 +78,27 @@ def logout():
 
 @app.route('/todo/<id>', methods=['GET'])
 def todo(id):
-    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s'" % id)
+    if not session.get('logged_in'):
+        return redirect('/login')
+    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s' AND user_id = '%s'" % (id, session['user']['id']))
     todo = cur.fetchone()
+    if not todo:
+        return redirect('/')
     return render_template('todo.html', todo=todo)
 
 
-@app.route('/todo', methods=['GET'])
-@app.route('/todo/', methods=['GET'])
-def todos():
+@app.route('/todo/', defaults={'page':1}, methods=['GET'])
+@app.route('/todo/page/<int:page>', methods=['GET'])
+def todos(page):
     if not session.get('logged_in'):
         return redirect('/login')
-    cur = g.db.execute("SELECT * FROM todos")
+    cur = g.db.execute("SELECT * FROM todos WHERE user_id = '%s'" % session['user']['id'])
     todos = cur.fetchall()
-    return render_template('todos.html', todos=todos)
+    count = len(todos)
+    if not todos and page != 1:
+        abort(404)
+    pagination = Pagination(page, PER_PAGE, count)
+    return render_template('todos.html', pagination=pagination, todos=todos)
 
 
 @app.route('/todo', methods=['POST'])
@@ -67,7 +107,7 @@ def todos_POST():
     if not session.get('logged_in'):
         return redirect('/login')
     g.db.execute(
-        "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
+        "INSERT INTO todos (user_id, description) VALUES ('%s', '%s') "
         % (session['user']['id'], request.form.get('description', ''))
     )
     g.db.commit()
@@ -94,8 +134,12 @@ def todo_delete(id):
 
 @app.route('/todo/<id>/json', methods=['GET'])
 def todo_json(id):
-    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s'" % id)
+    if not session.get('logged_in'):
+        return redirect('/login')
+    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s' AND user_id = '%s'" % (id, session['user']['id']))
     todo = cur.fetchone()
+    if not todo:
+        return redirect('/')
     data = [{'id': todo['id'], 'user_id': todo['user_id'], 'description': todo['description'], 'done': todo['done']}]
     result = json.dumps(data)
     return result
